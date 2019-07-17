ee7eabfac00b067ae206fcde50b1727253345068
surpreet.mundra <surpreet.mundra@gmail.com>
2019-07-17 16:10:07 +0000

    linking the create and show recipe pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,30 @@
 class RecipeController < ApplicationController
 
     get "/recipes" do
+      if logged_in?
       erb :"recipes/recipes"
+        else
+        redirect to "/"
+      end
     end
 
     get "/recipes/new" do
+      if logged_in?
       erb :"recipes/create_recipe"
+        else
+          redirect to "/"
+      end
     end
 
     post "/recipes" do
-      @recipe = Recipe.create(params)
+      recipe = Recipe.create(params)
+      redirect to "/recipe/#{recipe.id}"
+    end
+
+    get "/recipe/:id" do
+      @recipe = Recipe.find_by_id(params[:id])
       erb :"recipes/show"
     end
+
 
 end
