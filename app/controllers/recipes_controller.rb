5a9803d52d086463ad016953205c4ab7bf5266e8
surpreet.mundra <surpreet.mundra@gmail.com>
2019-07-17 17:04:14 +0000

    blank recipe field entries cannot be persisted

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipeController < ApplicationController
       if logged_in?
       erb :"recipes/recipes"
         else
-        redirect to "/"
+        redirect to "/login"
       end
     end
 
@@ -12,18 +12,41 @@ class RecipeController < ApplicationController
       if logged_in?
       erb :"recipes/create_recipe"
         else
-          redirect to "/"
+          redirect to "/login"
       end
     end
 
     post "/recipes" do
-      recipe = current_user.recipes.create(params)
-      redirect to "/recipe/#{recipe.id}"
+      if logged_in?
+        if params[:name] == "" || params[:ingredients] == "" || params[:instructions] == ""
+          redirect to "/recipes/new"
+        else
+          recipe = current_user.recipes.create(params)
+          redirect to "/recipe/#{recipe.id}"
+        end
+        redirect to "/login"
+      end
     end
 
     get "/recipe/:id" do
+      if logged_in?
       @recipe = Recipe.find_by_id(params[:id])
       erb :"recipes/show"
+      else
+        redirect to "/login"
+      end
+    end
+
+    get "/recipe/:id/edit" do
+      if logged_in?
+      @recipe = Recipe.find_by_id(params[:id])
+        if current_user = @recipe.user
+          erb :"recipes/edit"
+        else
+          redirect to "/recipes"
+        end
+        redirect to "/login"
+      end
     end
 
 
